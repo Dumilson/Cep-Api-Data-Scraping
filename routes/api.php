983b05de05f69a46b3c2a118ca65e1bd56317198
Dumilson <=>
2022-07-06 15:10:55 +0100

    application routes

---
+++ routes/api.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\Cep\CepController;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::group([
+	'prefix' => 'cep'
+], function(){
+	Route::get('/', [CepController::class, 'index']);
+	Route::delete('/{id}', [CepController::class, 'destroy']);
+	Route::get('/{id?}', [CepController::class, 'get']);
+	Route::put('/', [CepController::class, 'update']);
+});
